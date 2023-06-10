9e4d82af8c39eb444955942921b27ed3782956d1
uestebanv <ulisesestebanvaldes98@gmail.com>
2023-06-09 19:08:32 -0600

    Add project files.

---
+++ ML/Tienda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Tienda
+    {
+        public int IdTienda { get; set; }
+        public string Name { get; set; }
+        public ML.RazonSocial RazonSocial { get; set; }
+        public int Num { get; set; }
+        public string Calle { get; set; }
+        public string Domicilio { get; set; }
+        public string Municipio { get; set; }
+        public string Estado { get; set; }
+    }
+}
